1a3d83802d0968a78f51b1661262f4ce2dda795b
Nicolas Weltzel <nwelt13@gmail.com>
2023-01-16 11:50:59 +0100

    init

---
+++ src/main/java/de/volkswagen/UnserFilmabend/UnserFilmabendApplication.java
@@ -0,0 +1,13 @@
+package de.volkswagen.UnserFilmabend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnserFilmabendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnserFilmabendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/volkswagen/UnserFilmabend/UnserFilmabendApplicationTests.java
@@ -0,0 +1,13 @@
+package de.volkswagen.UnserFilmabend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnserFilmabendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
