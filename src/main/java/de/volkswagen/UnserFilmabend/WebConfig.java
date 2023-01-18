8c49d48c5a4a3406ae618f1ef6f3c087b7b6ddcf
Christopher Hoppe <christopher.hoppe@volkswagen.de>
2023-01-18 09:23:03 +0100

    test second remote repo

--- src/main/java/de/volkswagen/UnserFilmabend/WebConfig.java
+++ src/main/java/de/volkswagen/UnserFilmabend/WebConfig.java
@@ -1,17 +1,30 @@
 package de.volkswagen.UnserFilmabend;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import java.util.Arrays;
 
 @Configuration
-@EnableWebMvc
-public class WebConfig implements WebMvcConfigurer {
+public class WebConfig extends WebMvcConfigurerAdapter {
 
-    @Override
-    public void addCorsMappings(final CorsRegistry registry) {
-        registry.addMapping("/**");
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("*"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "OPTIONS", "DELETE", "PUT", "PATCH"));
+        configuration.setAllowedHeaders(Arrays.asList("X-Requested-With", "Origin", "Content-Type", "Accept", "Authorization"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
+
 
 }
--- src/main/java/de/volkswagen/UnserFilmabend/controller/AccountController.java
+++ src/main/java/de/volkswagen/UnserFilmabend/controller/AccountController.java
@@ -4,9 +4,7 @@ import de.volkswagen.UnserFilmabend.dto.LoginData;
 import de.volkswagen.UnserFilmabend.model.Account;
 import de.volkswagen.UnserFilmabend.service.AccountService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class AccountController {
